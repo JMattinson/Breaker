8ba620561b3b484626c6040e57cd4bfe2b6e8a90
Jaden M <Jadenmattinson6@gmail.com>
2023-08-31 22:46:15 -0600

    Implemented basic paddle movement

--- Breaker/Assets/Scripts/FollowTouchBehavior.cs
+++ Breaker/Assets/Scripts/FollowTouchBehavior.cs
@@ -1,23 +1,42 @@
-using System;
 using UnityEngine;
-using UnityEngine.InputSystem.EnhancedTouch;
-using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
+using UnityEngine.InputSystem;
+
 
 public class FollowTouchBehavior : MonoBehaviour
 {
-    private void Awake()
-    {
-        EnhancedTouchSupport.Enable();
-        
-    }
+   public GameObject followTarget;
+   public PlayerInput playerInput;
+   private InputAction touchPositionAction;
+   private InputAction touchPressAction;
 
-    private void Update()
-    {
-        if (Touch.activeFingers.Count == 1)
-        {
-            Touch activeTouch = Touch.activeFingers[0].currentTouch;
-            Debug.Log($"Phase: {activeTouch.phase} | Position: {activeTouch.startScreenPosition}");
-        }
+   private void Awake()
+   {
+      playerInput = GetComponent<PlayerInput>();
+      print(playerInput);
+      touchPressAction = playerInput.actions.FindAction("TouchPress");
+      touchPositionAction = playerInput.actions.FindAction("TouchPosition");
+       
+   }
 
-    }
+   private void OnEnable()
+   {
+      touchPressAction.performed += TouchPressed;
+   }
+
+   private void OnDisable()
+   {
+      touchPressAction.performed -= TouchPressed;
+   }
+
+   private void TouchPressed(InputAction.CallbackContext context)
+   {
+      Vector3 position = touchPositionAction.ReadValue<Vector2>();
+      position.z = -30;
+      position = Camera.main.ScreenToWorldPoint(position);
+      position.y = followTarget.transform.position.y;
+      position.x = position.x * -1;
+      
+      position.z = followTarget.transform.position.z;
+      followTarget.transform.position = position;
+   }
 }
